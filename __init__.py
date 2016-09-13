c8174cfb3966c01db18786f08d1b7a3d07a65e2d
sergi_rubio <sergi_rubio@3b39871e-fe4b-0410-b5db-e82997f68959>
2016-09-13 18:23:53 +0000

    Adding package structure

--- __init__.py
+++ __init__.py
@@ -1,5 +1,2 @@
-from PySignalSimulator import *
-from fandango.dynamic import DynamicDS,DynamicDSClass
-from fandango.interface import FullTangoInheritance
-PySignalSimulator,PySignalSimulatorClass = FullTangoInheritance('PySignalSimulator',PySignalSimulator,PySignalSimulatorClass,DynamicDS,DynamicDSClass,ForceDevImpl=True)
- 
+
+from SimulatorDS import *
